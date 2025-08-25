36931d92d96d5c1d539844bf5a508ee2a11556ef
Matt Wilson <152443343+mwsis@users.noreply.github.com>
2025-08-25 15:26:34 +1000

    MinGW (#18)
    * **prepare_cmake.sh** : automatic selection of '--mingw' flag
    
    * dependency fix
    
    * automatic detection of MinGW make command

--- prepare_cmake.sh
+++ prepare_cmake.sh
@@ -4,13 +4,20 @@ ScriptPath=$0
 Dir=$(cd $(dirname "$ScriptPath"); pwd)
 Basename=$(basename "$ScriptPath")
 CMakeDir=${SIS_CMAKE_BUILD_DIR:-$Dir/_build}
-MakeCmd=${SIS_CMAKE_COMMAND:-make}
+if [[ -n "$MSYSTEM" ]]; then
 
+	DefaultMakeCmd=mingw32-make.exe
+	MinGW=1
+else
+
+	DefaultMakeCmd=make
+fi
+MakeCmd=${SIS_CMAKE_MAKE_COMMAND:-${SIS_CMAKE_COMMAND:-$DefaultMakeCmd}}
 
 Configuration=Release
 ExamplesDisabled=0
 MSVC_MT=0
-MinGW=0
+MinGW="${MinGW:=0}"
 RunMake=0
 STLSoftDirGiven=
 TestingDisabled=0
@@ -60,7 +67,7 @@ while [[ $# -gt 0 ]]; do
 
       cat << EOF
 Diagnosticism is a standalone library of simple components for aiding in diagnostics for C and C++ projects
-Copyright (c) 2024, Matthew Wilson and Synesis Information Systems
+Copyright (c) 2025, Matthew Wilson and Synesis Information Systems
 Creates/reinitialises the CMake build script(s)
 
 $ScriptPath [ ... flags/options ... ]
