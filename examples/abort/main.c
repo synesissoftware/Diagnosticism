c74fc7213f4721ff0a0e11a17f92ba0857a67f68
Matt Wilson <matthew@synesis.com.au>
2026-08-04 21:06:51 +1000

    fix

--- examples/abort/main.c
+++ examples/abort/main.c
@@ -3,37 +3,56 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
 
 int main(int argc, char* argv[])
 {
+    char                time_buf[64];
     time_t              t;
-    struct tm const*    tm;
-    char const*         asc;
+    struct tm           tm;
     size_t              n;
 
     ((void)&argc);
     ((void)&argv);
 
     time(&t);
-    tm = localtime(&t);
-    asc = asctime(tm);
-    n = strlen(asc);
 
-    if ('\n' == asc[n - 1])
+#if 0
+#elif defined(_MSC_VER) &&\
+      _MSC_VER >= 1400
+
+    if (0 != localtime_s(&tm, &t))
     {
-        --n;
+        return EXIT_FAILURE;
+    }
+#else /* ? _MSC_VER */
+
+    {
+        struct tm const* const tm_p = localtime(&t);
+
+        if (NULL == tm_p)
+        {
+            return EXIT_FAILURE;
+        }
+
+        tm = *tm_p;
+    }
+#endif /* _MSC_VER */
+
+    n = strftime(&time_buf[0], sizeof(time_buf), "%c", &tm);
+
+    if (0 == n)
+    {
+        return EXIT_FAILURE;
     }
 
     fprintf(stdout, "calling `diagnosticism_abort()` ...\n");
 
-    diagnosticism_abortf(stderr, "ex-abort", "time to bug out: %.*s", (int)n, asc);
+    diagnosticism_abortf(stderr, "ex-abort", "time to bug out: %s", time_buf);
 
     return EXIT_SUCCESS;
 }
 
 
 /* ///////////////////////////// end of file //////////////////////////// */
-
