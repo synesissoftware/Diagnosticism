a3f4c11309f84ef659e7a6b415830491bad33b6a
Matt Wilson <matthew@synesis.com.au>
2026-08-04 21:18:26 +1000

    layout

--- cmake/BuildType.cmake
+++ cmake/BuildType.cmake
@@ -1,4 +1,15 @@
 
+# ######################################################################## #
+# File:     /cmake/BuildType.cmake
+#
+# Purpose:  CMake module file (for BuildType)
+#
+# Created:  16th October 2019
+# Updated:  12th January 2024
+#
+# ######################################################################## #
+
+
 # Including this module sets the `CMAKE_BUILD_TYPE` value as follows:
 #
 #  1. If user specifies on the command line, then `CMAKE_BUILD_TYPE` already
@@ -25,24 +36,27 @@ include(BuildType)
 
 
 if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
-	set(default_build_type "Debug")
+
+    set(default_build_type "Debug")
 else()
-	set(default_build_type "Release")
+
+    set(default_build_type "Release")
 endif()
 
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
 
-	message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
+    message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
 
-	set(CACHE CMAKE_BUILD_TYPE "${default_build_type}"
-		STRING "Choose the type of build." FORCE
-	)
+    set(CACHE CMAKE_BUILD_TYPE "${default_build_type}"
+        STRING "Choose the type of build." FORCE
+    )
 
-	# Set the possible values of build type for cmake-gui
-	set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
-		"Debug" "Release" "MinSizeRel" "RelWithDebInfo"
-	)
+    # Set the possible values of build type for cmake-gui
+    set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
+        "Debug" "Release" "MinSizeRel" "RelWithDebInfo"
+    )
 endif()
+
 
 # ############################## end of file ############################# #
 
